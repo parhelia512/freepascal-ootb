9628ae8bae2057e94ffc0d53074a67399a3f71b2
fredvs <fiens@hotmail.com>
2026-04-29 01:24:25 +0200

    Updated build script for DragonflyBSD64.

--- build_fpc-ootb-64_dragonflybsd.sh
+++ build_fpc-ootb-64_dragonflybsd.sh
@@ -14,13 +14,15 @@ mkdir /tmp/fpc-ootb/usr
 gmake all FPC=$COMPILER OPT="-Fl/usr/local/lib" 
 gmake FPC=$COMPILER install INSTALL_PREFIX=$INSTALLDIR
 
+$COMPILER -Mfpc -Scgi -CX -Cg -Os1 -Xs -XX -l -vewnhibq -Fipackages/ide -Ficompiler -Ficompiler/x86_64 -Fucompiler -Fucompiler/x86 -Fucompiler/x86_64 -Fucompiler/systems -Fupackages/rtl-extra/src/inc -Fupackages/fv/src -Fupackages/ide/ -FUpackages/ide/ -FEpackages/ide/ -opackages/ide/fp -dx86_64 -dnodebug -B packages/ide/fp.pas 
+
 rm -f -r ./fpc-ootb-64
 mkdir ./fpc-ootb-64
 mkdir ./fpc-ootb-64/lib
 mkdir ./fpc-ootb-64/license
 mkdir ./fpc-ootb-64/tools
 mkdir ./fpc-ootb-64/units
-mkdir ./fpc-ootb-64/units/x86_64-dragonflybsd
+mkdir ./fpc-ootb-64/units/x86_64-dragonfly
 
 cp ./ootb/fp.cfg ./fpc-ootb-64/
 cp ./ootb/fp.ini ./fpc-ootb-64/
@@ -30,15 +32,16 @@ cp ./ootb/readme.txt ./fpc-ootb-64/
 cp -rf ./license ./fpc-ootb-64
 
 cp /tmp/fpc-ootb/usr/lib/fpc/3.2.4/ppcx64 ./fpc-ootb-64/fpc-ootb-64
-cp /tmp/fpc-ootb/usr/bin/fp ./fpc-ootb-64/fpide-ootb-64
+cp ./packages/ide/fp ./fpc-ootb-64/fpide-ootb-64
+#cp /tmp/fpc-ootb/usr/bin/fp ./fpc-ootb-64/fpide-ootb-64
 cp /tmp/fpc-ootb/usr/lib/libpas2jslib.so ./fpc-ootb-64/lib/
-cp -rf /tmp/fpc-ootb/usr/lib/fpc/3.2.4/units/x86_64-dragonflybsd ./fpc-ootb-64/units
+cp -rf /tmp/fpc-ootb/usr/lib/fpc/3.2.4/units/x86_64-dragonfly ./fpc-ootb-64/units
 cp /tmp/fpc-ootb/usr/bin/fpcres ./fpc-ootb-64/tools/
 cp /tmp/fpc-ootb/usr/bin/h2pas ./fpc-ootb-64/tools/
 cp /tmp/fpc-ootb/usr/bin/fpcjres ./fpc-ootb-64/tools/
 cp /tmp/fpc-ootb/usr/bin/pas2js ./fpc-ootb-64/tools/
 
 tar -zcvf fpc-ootb-324-x86_64-dragonflybsd.tar.gz ./fpc-ootb-64
-rm -f -r /tmp/fpc-ootb
+#rm -f -r /tmp/fpc-ootb
 #rm -f -r ./fpc-ootb-64
 
