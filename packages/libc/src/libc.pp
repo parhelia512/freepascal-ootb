00686edaa4758c954dbaaa613837d2ebf21d4b6f
fredvs <fiens@hotmail.com>
2026-05-10 02:22:38 +0200

    Synchro with fpc fixes_3.2 of 09/05/2026 + update libc package.

--- packages/libc/src/libc.pp
+++ packages/libc/src/libc.pp
@@ -12,6 +12,27 @@ Interface
 
 uses unixtype,kerneldefs;
 
+const
+  // allow to assign proper signed symbol table name for a libc.so.6 method
+  {$if defined(linux) and defined(cpux86_64)}
+  LIBC_SUFFIX = '@GLIBC_2.2.5';
+  {$else}
+  {$if defined(linux) and defined(cpuaarch64)}
+  LIBC_SUFFIX = ''; //  '@GLIBC_2.17'
+  {$else}
+  {$if defined(linux) and defined(cpuarm)}
+  LIBC_SUFFIX =  '@GLIBC_2.4';
+  {$else}
+  {$if defined(linux) and defined(cpui386)}
+  LIBC_SUFFIX = '@GLIBC_2.0';
+  {$else}
+  LIBC_SUFFIX = '';
+  {$endif}
+  {$endif}
+  {$endif}
+  {$endif}
+  
+
 Const
   clib = 'c';
   dllib = 'dl';
